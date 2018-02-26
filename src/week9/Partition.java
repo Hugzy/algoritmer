d7b059bc3a5a1247065823d3e106fae92de834d1
Hugzy <frede.madsen3@gmail.com>
2018-02-26 18:01:25 +0100

    init commit for week 9

---
+++ src/week9/Partition.java
@@ -0,0 +1,18 @@
+package week9;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Partition {
+
+    public static void main(String[] args) {
+        List<Integer> numbersToSort;
+        numbersToSort = Arrays.asList(13,19,9,5,12,8,7,4,21,2,6,11);
+        System.out.println(numbersToSort);
+    }
+
+    private void Partition(){
+
+    }
+
+}
